ee98badaac9215ee1c99371c1ed6a16f55dcef8a
Rohit Agrawal <rohitagrawalofficialmail@gmail.com>
2021-06-06 02:18:33 +0530

    Added live trading, added CoinDCX account wrapper, Added live plotting and updating of indicators, some refactors

--- moonlander/strategy/__init__.py
+++ moonlander/strategy/__init__.py
@@ -1,13 +1,16 @@
+from datetime import datetime, timedelta
 import pandas as pd
 import warnings
 import bokeh
 import time
 import numpy as np
 from tqdm.auto import tqdm
+
 from bokeh.layouts import gridplot
+from bokeh.io import show, push_notebook
 
 from ..viz import create_candle_plot
-from ..account import LocalAccount
+from ..account.local import LocalAccount
 
 def percent_change(d1, d2):
     return (d2 - d1) / d1
@@ -29,21 +32,37 @@ class TradingStrategyBase:
         self.data = asset.data.reset_index()
         self.indicators = []
 
-        # Plotting
-        self.candle_plot, _ = create_candle_plot(self.asset, fig_height=400, colored=False)
-
     def setup(self):
         pass
 
-    def backtest(self, amount = 1000):
-        tracker = []
-        account = LocalAccount(amount)
-
-        self.setup()
-
+    def compute_indicators(self):
         # Compute values for all indicators
         for indicator in self.indicators:
             indicator.compute(self.data)
+
+    def update_indicators(self):
+        # Compute values for all indicators
+        for indicator in self.indicators:
+            indicator.update(self.data)
+            indicator.lookback = indicator.values
+
+    def backtest(self, amount = 1000, start_date = None, relative_lookback_size = None, commision = 0, plot_results = True, show_trades = True):
+        tracker = []
+        account = LocalAccount(amount, commision = commision)
+
+        # Setting custom backtest sizes
+        self.data = self.asset.data.reset_index()
+
+        if start_date is not None:
+            self.data = self.asset.data.loc[start_date:].reset_index()
+
+        elif relative_lookback_size is not None:
+            self.data = self.data.iloc[-relative_lookback_size:].reset_index(drop = True)
+
+        self.setup()
+        self.compute_indicators()
+
+        print('Performing backtest from:', pd.to_datetime(self.data.timestamp.values[0]).strftime('%d %B, %Y (%H:%M:%S)'), 'to', pd.to_datetime(self.data.timestamp.values[-1]).strftime('%d %B, %Y (%H:%M:%S)'))
 
         # Enter backtest ---------------------------------------------  
         for index, today in tqdm(self.data.iterrows(), total = self.data.shape[0]):
@@ -75,9 +94,9 @@ class TradingStrategyBase:
 
         # ------------------------------------------------------------
 
-        self.backtest_results(account)
+        self.backtest_results(account, plot_results, show_trades)
 
-    def backtest_results(self, account, plot_performance = True):
+    def backtest_results(self, account, plot_performance = True, show_trades = True):
         print("-------------- Results ----------------\n")
         being_price = self.data.iloc[0]['open']
         final_price = self.data.iloc[-1]['close']
@@ -85,7 +104,7 @@ class TradingStrategyBase:
         strategy_returns = percent_change(account.initial_capital, account.total_value(final_price))
         buyhold_returns = percent_change(being_price, final_price)
 
-        print("Relative performance: {0}%".format(round((strategy_returns - buyhold_returns) * 100 / buyhold_returns, 2)))
+        print("Relative performance: {0}%".format(round(abs((strategy_returns - buyhold_returns) * 100 / buyhold_returns), 2)))
         print()
 
         print("Strategy     : {0}%".format(round(strategy_returns*100, 2)))
@@ -106,9 +125,9 @@ class TradingStrategyBase:
         print("\n---------------------------------------")
 
         if plot_performance:
-            self.chart(account, show_trades=True)
+            self.chart(account, show_trades=show_trades)
 
-    def chart(self, account, show_positions=True, show_trades=True, title="Equity Curve"):
+    def chart(self, account, colored = False, show_positions=True, show_trades=True, title="Equity Curve"):
         """Chart results.
         :param show_trades: Show trades on plot
         :type show_trades: bool
@@ -116,6 +135,9 @@ class TradingStrategyBase:
         :type title: str
         """     
 
+        # Plot candle plot
+        self.candle_plot, _ = create_candle_plot(self.asset, fig_height=400, colored=colored)
+        
         # Plot equity curve
         p = bokeh.plotting.figure(x_axis_type="datetime", plot_width=1000, plot_height=300, title=title, x_range = self.candle_plot.x_range)
         p.grid.grid_line_alpha = 0.3
@@ -124,15 +146,17 @@ class TradingStrategyBase:
         shares = account.initial_capital/self.data.iloc[0]['open']
         base_equity = [price*shares for price in self.data['close']]     
 
-        p.line(self.data['timestamp'], base_equity, color='#CAD8DE', legend='Buy and Hold')
-        p.line(self.data['timestamp'], account.equity, color='#49516F', legend='Strategy')
+        p.line(self.data['timestamp'], base_equity, color='#CAD8DE', legend_label='Buy and Hold')
+        p.line(self.data['timestamp'], account.equity, color='#49516F', legend_label='Strategy')
         p.legend.location = "top_left"
 
-        final_plot_layout = [p, self.candle_plot]
+        final_plot_layout = [p]
 
+        indicator_plots = [self.candle_plot]
         # Plot Indicators
         for indicator in self.indicators:
-            indicator.plot_indicator(final_plot_layout)
+            indicator_plots = indicator.plot_indicator(indicator_plots)
+        final_plot_layout += indicator_plots
 
         if show_positions:
             for position in account.positions:
@@ -150,7 +174,7 @@ class TradingStrategyBase:
                     if trade.type == 'buy': 
                         final_plot_layout[0].circle(trade.date, y, size=6, color='green', alpha=0.5)
 
-                        final_plot_layout[1].circle(trade.date, trade.price, size=8, color='green', alpha=0.5, legend = 'Buy order')
+                        final_plot_layout[1].circle(trade.date, trade.price, size=8, color='green', alpha=0.5, legend_label = 'Buy order')
                         # long_pos = bokeh.models.Span(location=trade.date,
                         #       dimension='height', line_color='green', line_alpha = 0.5,
                         #       line_dash='dashed', line_width=2)
@@ -159,7 +183,7 @@ class TradingStrategyBase:
                     elif trade.type == 'sell': 
                         final_plot_layout[0].circle(trade.date, y, size=6, color='red', alpha=0.5)
 
-                        final_plot_layout[1].circle(trade.date, trade.price, size=8, color='red', alpha=0.5, legend = 'Sell order')
+                        final_plot_layout[1].circle(trade.date, trade.price, size=8, color='red', alpha=0.5, legend_label = 'Sell order')
                         # long_pos = bokeh.models.Span(location=trade.date,
                         #       dimension='height', line_color='red', line_alpha = 0.5,
                         #       line_dash='dashed', line_width=2)
@@ -169,3 +193,70 @@ class TradingStrategyBase:
                     pass
             
         return bokeh.plotting.show(gridplot(final_plot_layout, ncols = 1))
+
+    def execute(self, trading_account, max_data_points = 1000):
+        self.setup()
+        self.compute_indicators()
+
+        candle_plot, volume_plot = create_candle_plot(self.asset, fig_height=400)
+        # Inital plot with candles and indicators
+        final_plot_layout = []
+
+        indicator_plots = [candle_plot, volume_plot]
+
+        # Plot Indicators
+        for indicator in self.indicators:
+            indicator_plots = indicator.plot_indicator(indicator_plots)
+        final_plot_layout += indicator_plots
+
+        stream_plot = show(gridplot(final_plot_layout, ncols = 1), notebook_handle = True)
+
+        self.trading_account = trading_account
+
+        def on_new_candle(candle):
+            print('{} | Plotting new candle ...'.format(candle['timestamp']))
+            print(candle)
+
+            new_candle_dict=dict(
+                timestamp=[candle['timestamp']],
+                low=[candle['low']],
+                high=[candle['high']],
+                open=[candle['open']],
+                close=[candle['close']],
+                volume=[candle['volume']]
+            )
+
+            if candle['close'] > candle['open']:
+                self.asset._data_source_increasing.stream(new_candle_dict)
+            else:
+                self.asset._data_source_decreasing.stream(new_candle_dict)
+
+
+            new_scaling_source=dict(
+                timestamp=[candle['timestamp']],
+                low=[candle['low']],
+                high=[candle['high']]
+            )
+            self.asset.scaling_source.stream(new_scaling_source)
+            
+
+            # Execute Live Trading Logic
+            new_candle_row_dict = dict(
+                timestamp = candle['timestamp'],
+                low = candle['low'],
+                high = candle['high'],
+                open = candle['open'],
+                close = candle['close'],
+                volume = candle['volume']
+            )
+            
+            self.data = self.data.append(new_candle_row_dict, ignore_index=True)
+            self.data = self.data.iloc[-1000:]
+            self.update_indicators()
+
+            push_notebook(handle=stream_plot)
+
+            self.trading_account.update_shares_and_balances()
+            self.logic(self.data, self.trading_account)
+
+        self.kline_stream = self.asset.provider.stream_klines(self.asset, new_candle_callback = on_new_candle)
